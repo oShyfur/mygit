c72bf2ca25e62baf25610435bd79e8ec2375a8c2
Shyfur <Shyfur@Me>
2023-02-28 00:01:22 +0600

    a Little Chnaged

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 <!DOCTYPE html>
 <html>
 	<body>
-
 		<h2>My Jenkin Test Today - ANOTHER NODE </h2>
-
 		<?php
 		// Set the new timezone
-		date_default_timezone_set('Asia/Kolkata');
+		date_default_timezone_set('Asia/Dhaka');
 		$date = date('d-m-y h:i:s');
 		echo $date;
 		?>
-
 	</body>
 </html>
 
